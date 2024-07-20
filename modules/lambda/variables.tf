c2ddb6a86b24e8697779be18f2b0764827110958
Marc Roelofs <marc@roelofsfamilie.nl>
2024-07-20 16:11:10 +0200

    variables all over the place

--- modules/lambda/variables.tf
+++ modules/lambda/variables.tf
@@ -1,10 +1,37 @@
 # Input variable definitions
 
+variable "aws_region" {
+  description = "AWS region for all resources."
+
+  type    = string
+  default = "eu-central-1"
+}
+
+variable "s3_bucket_prefix" {
+  description = "S3 bucket prefix"
+  type = string
+  default = "apigw-lambda-ddb"
+  
+}
+
+variable "dynamodb_table" {
+  description = "name of the ddb table"
+  type = string
+  default = "tf-news-item-table"
+  
+}
 
 variable "lambda_name" {
   description = "name of the lambda function"
   type = string
   default = "lampda-news-post"
+  
+}
+
+variable "apigw_name" {
+  description = "name of the lambda function"
+  type = string
+  default = "apigw-http-news"
   
 }
 
@@ -14,9 +41,8 @@ variable "lambda_log_retention" {
   default = 7
 }
 
-variable "dynamodb_table" {
-  description = "name of the ddb table"
-  type = string
-  default = "tf-news-item-table"
-  
+variable "apigw_log_retention" {
+  description = "api gwy log retention in days"
+  type = number
+  default = 7
 }
