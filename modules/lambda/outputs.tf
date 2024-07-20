1f31d0b4810a352eb460345b164e3fdbfb4c3946
Marc Roelofs <marc@roelofsfamilie.nl>
2024-07-20 16:25:53 +0200

    merged all of lambda in apigw

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -1,8 +1,3 @@
 
 # Output value definitions
 
-output "lambda_log_group" {
-  description = "Name of the CloudWatch logs group for the lambda function"
-
-  value = aws_cloudwatch_log_group.lambda_logs.id
-}
