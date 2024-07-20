d64e4487e37617d6ea623361a8964137b669ed80
Marc Roelofs <marc@roelofsfamilie.nl>
2024-07-20 16:47:35 +0200

    .

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
 
 # # Output value definitions
 
-output "apigwy_url" {
-  description = "URL for API Gateway stage"
+# output "apigwy_url" {
+#   description = "URL for API Gateway stage"
 
-  #value = aws_apigatewayv2_stage.default.invoke_url
-  value = aws_api_gateway_deployment.api_deployment
-}
+#   #value = aws_apigatewayv2_stage.default.invoke_url
+#   value = aws_api_gateway_deployment.api_deployment
+# }
 
 # output "lambda_log_group" {
 #   description = "Name of the CloudWatch logs group for the lambda function"
