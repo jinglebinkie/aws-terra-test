16f7853239dfa368b417b7bf9fa4ac41b2d3432d
Marc Roelofs <marc@roelofsfamilie.nl>
2024-07-20 16:20:41 +0200

    merging apigw and lambda

--- modules/apigw/main.tf
+++ modules/apigw/main.tf
@@ -102,3 +102,111 @@ resource "aws_lambda_function" "apigw_lambda_ddb" {
   depends_on = [aws_cloudwatch_log_group.lambda_logs]
   
 }
+
+
+#create a bucket
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket_prefix = var.s3_bucket_prefix
+  force_destroy = true
+}
+# set permission options so acl can be set private
+# resource "aws_s3_bucket_ownership_controls" "lambda_bucket_privs" {
+#   bucket = aws_s3_bucket.lambda_bucket.id
+#   rule {
+#     object_ownership = "BucketOwnerPreferred"
+#   }
+# }
+# # set acl to private access 
+# resource "aws_s3_bucket_acl" "private_bucket" {
+#   bucket = aws_s3_bucket.lambda_bucket.id
+#   acl    = "private"
+# }
+
+data "archive_file" "lambda_zip" {
+  type = "zip"
+
+  source_dir  = "${path.module}/src"
+  output_path = "${path.module}/src.zip"
+}
+
+resource "aws_s3_object" "lampda-bucket" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  key    = "src.zip"
+  source = data.archive_file.lambda_zip.output_path
+
+  etag = filemd5(data.archive_file.lambda_zip.output_path)
+}
+
+
+
+resource "aws_cloudwatch_log_group" "lambda_logs" {
+  name = "/aws/lambda/${var.lambda_name}-${random_string.random.id}"
+
+  retention_in_days = var.lambda_log_retention
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "LambdaDdbPost"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambda_exec_role" {
+  name = "lambda-tf-pattern-ddb-post"
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:GetItem",
+                "dynamodb:PutItem",
+                "dynamodb:Scan",
+                "dynamodb:UpdateItem"
+            ],
+            "Resource": "arn:aws:dynamodb:*:*:table/${var.dynamodb_table}"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_exec_role.arn
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.apigw_lambda_ddb.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*"
+}
+resource "random_string" "random" {
+  length           = 4
+  special          = false
+}
